9130f3c1170dcba445d452e82ef854c0b4bc72e8
Oliver Duzon <131516053+Oli-verse@users.noreply.github.com>
2025-08-23 23:44:49 +0800

    update on App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
 
-function Welcome(props) {
-  return <h1>Weather App</h1>;
+function App() {
+
+  //const url = API KEY + CITY NAME + UNITS + API KEY
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="top"></div>
+        <div className="location">
+          <p>Bacoor City</p>
+        </div>
+        <div className="temp">
+          <h1>30°C</h1>
+        </div>
+        <div className="description">
+          <p>Sunny</p>
+        </div>
+        <div className="bottom"></div>
+        <div className="feels">
+          <p>Feels like: 32°C</p> 
+          </div>
+        <div className="humidity">
+          <p>Humidity: 70%</p>   
+        </div>
+        <div className="wind">
+          <p>Wind speed: 10 km/h</p>  
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Welcome;
+export default App;
