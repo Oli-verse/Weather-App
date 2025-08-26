3496fa72320e2229acbc1b226433ff30f8753b17
Oliver Duzon <131516053+Oli-verse@users.noreply.github.com>
2025-08-26 23:19:02 +0800

    update on css

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
 import React from 'react';
+import cloudyVideo from './resources/cloudy.mp4';
+
 
 function App() {
-
+  
   //const API_KEY = process.env.REACT_APP_WEATHER_KEY;
 
   return (
     <div className="App">
-      <div className="container">
-        <div className="top"></div>
-        <div className="location">
-          <p>Bacoor City</p>
-        </div>
-        <div className="temp">
-          <h1>30°C</h1>
-        </div>
-        <div className="description">
-          <p>Sunny</p>
-        </div>
-        <div className="bottom"></div>
-        <div className="feels">
-          <p>Feels like: 32°C</p> 
+       <video autoPlay loop muted playsInline className="background-video">
+          <source src={require('./resources/cloudy.mp4')} type="video/mp4" />
+        </video>
+
+
+      <div className="card-container">
+        <div className="glass-card top-card">
+          <div className="location">
+            <p>Bacoor City</p>
           </div>
-        <div className="humidity">
-          <p>Humidity: 70%</p>   
+          <div className="temp">
+            <h1>30°C</h1>
+          </div>
+          <div className="description">
+            <p>cloudy</p>
+          </div>
         </div>
-        <div className="wind">
-          <p>Wind speed: 10 km/h</p>  
+
+        <div className="glass-card bottom-card">
+          <div className="feels">
+            <p>Feels like: 32°C</p> 
+          </div>
+          <div className="humidity">
+            <p>Humidity: 70%</p>   
+          </div>
+          <div className="wind">
+            <p>Wind speed: 10 km/h</p>  
+          </div>
         </div>
       </div>
     </div>
